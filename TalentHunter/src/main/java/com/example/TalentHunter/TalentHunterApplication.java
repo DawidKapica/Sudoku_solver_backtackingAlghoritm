deb62d663f042785739fccb390482b3f884b9efb
Weronika <weronikababicz1@gmail.com>
2020-03-23 01:05:04 +0100

    init

---
+++ TalentHunter/src/main/java/com/example/TalentHunter/TalentHunterApplication.java
@@ -0,0 +1,13 @@
+package com.example.TalentHunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentHunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentHunterApplication.class, args);
+	}
+
+}
---
+++ TalentHunter/src/test/java/com/example/TalentHunter/TalentHunterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TalentHunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentHunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
